23b264730879b6ccf585c167f717d05d270fe202
126KshitijSingh <124625374+126KshitijSingh@users.noreply.github.com>
2025-07-12 16:47:54 +0530

    Create Program-1.cpp

---
+++ Program-1.cpp
@@ -0,0 +1,40 @@
+## Problem-1: Create a small calculator which performs operations such as Addition, Subtraction, Multiplication and Division using class.
+#include <iostream>     
+#include <string>   
+
+using namespace std;
+
+int main()
+{
+    double a, b;
+    string ops, ans;
+
+    cout << "First number: ";
+    cin >> a;
+
+    cout << "Second number: ";
+    cin >> b;
+
+    cout << "Choose operator '+', '-', '*', '/': ";
+    cin >> ops;
+
+    if (ops == "+") {
+        ans = to_string(a + b);
+    } else if (ops == "-") {
+        ans = to_string(a - b);
+    } else if (ops == "*") {
+        ans = to_string(a * b);
+    } else if (ops == "/") {
+        if (b != 0) {
+            ans = to_string(a / b);
+        } else {
+            ans = "Error: Division by zero!";
+        }
+    } else {
+        ans = "Invalid operation!";
+    }
+
+    cout << "Result: " << ans << endl;
+
+    return 0;
+}
